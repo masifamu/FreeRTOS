3f7f7eae8ffaa5265548590a7c6b4a0b75dd59eb
masifamu <asif@syntiant.com>
2023-04-01 20:19:04 +0530

    chenged to freeRTOS kernel

--- 0_converting_existing_sketch/0_converting_existing_sketch.ino
+++ 0_converting_existing_sketch/0_converting_existing_sketch.ino
@@ -1,17 +1,32 @@
+#include <Arduino_FreeRTOS.h>
+
 #define YELLOW 7
 #define GREEN  6
 #define BLUE   8
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(YELLOW, OUTPUT);
-  pinMode(GREEN, OUTPUT);
-  pinMode(BLUE, OUTPUT);
+  xTaskCreate(yellowLedControllerTask, "YellowLED",128,NULL,1,NULL);
+  xTaskCreate(greenLedControllerTask, "greenLED",128,NULL,1,NULL);
+  xTaskCreate(blueLedControllerTask, "blueLED",128,NULL,1,NULL);
 }
-
+void yellowLedControllerTask(void *pvParameters){
+  pinMode(YELLOW, OUTPUT);
+  while(1){
+    digitalWrite(YELLOW, digitalRead(YELLOW)^1);      
+  }
+}
+void greenLedControllerTask(void *pvParameters){
+  pinMode(GREEN, OUTPUT);
+  while(1){
+    digitalWrite(GREEN, digitalRead(GREEN)^1);
+  }
+}
+void blueLedControllerTask(void *pvParameters){
+  pinMode(BLUE, OUTPUT);
+  while(1){
+    digitalWrite(BLUE, digitalRead(BLUE)^1);
+  }
+}
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(YELLOW, HIGH);
-  digitalWrite(GREEN, HIGH);
-  digitalWrite(BLUE, HIGH);
 }
